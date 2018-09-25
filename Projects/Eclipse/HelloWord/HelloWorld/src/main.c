05fa91ca883d7dac66813ccedfc3c976c0ba8acd
Vinay YJ <42695824+vinayyj@users.noreply.github.com>
2018-09-25 11:20:17 +0530

    Added Tools and Sample Project

---
+++ Projects/Eclipse/HelloWord/HelloWorld/src/main.c
@@ -0,0 +1,32 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Vinay YJ
+ Version     :
+ Copyright   : Copyright (c) 2018 Vinay YJ
+ Description : Hello World in C
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+/*
+ *
+ * Print a greeting message on standard output and exit.
+ *
+ * On embedded platforms this might require semi-hosting or similar.
+ *
+ * For example, for toolchains derived from GNU Tools for Embedded,
+ * to enable semi-hosting, the following was added to the linker:
+ *
+ * --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lc -lm -lrdimon -Wl,--end-group
+ *
+ * Adjust it for other toolchains.
+ *
+ */
+
+int main(void)
+{
+  printf("Hello ARM World!" "\n");
+  return 0;
+}
--- Tools/Programming/Readme.txt
+++ Tools/Programming/Readme.txt
@@ -1,1 +1,9 @@
-Download Latest version of Eclipse IDE with c/c++ Developers
+1. Download Latest version of Eclipse IDE with c/c++ Developers
+
+    Or
+
+   Use the cpp-2018-09/eclipse on the Tools folder to run eclipse c/c++ directly
+
+2. Install GNU Extension from Eclipse Marketplace
+
+3. Use the ARM Toolchain while building the project
